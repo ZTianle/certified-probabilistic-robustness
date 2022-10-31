5c099f75f0b12272b8b96151ac5c71a10ff2335c
ZTianle <zhangtianle95@gmail.com>
2022-10-31 17:02:24 +0000

    add dataset cifar100

--- train_cifar100.sh
+++ train_cifar100.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 # training
 
-#python -m robustness.main \
-#       --dataset "cifar" \
-#       --adv-train 1 \
-#       --adv-eval 1 \
-#       --tries 1 \
-#       --spatial-constraint 45 \
-#       --use-best 0 \
-#       --attack-type "random" \
-#       --out-dir ./standard/retrain \
-#       --arch "resnet18" \
-#       --resume "./standard/logs/checkpoints/dir/0e371243-5e1b-4f41-a40b-4eed8d349c17/checkpoint.pt.best" \
-#       --data "./path/to/cifar" \
-#       --batch-size 128 
-
-      # --hue 1.5707963268\
-
 out=./training/standard/
 dataset="cifar100"
 arch="resnet18"
@@ -25,7 +9,7 @@ transform_type="semantic spatial color blur"
 for transform in $transform_type
 do
 echo $transform
-out_dir=$out$dataset/$arch/$transform/no_aug
+out_dir=$out$dataset/$arch/$transform/aug
 echo $out_dir
 echo $dataset
 echo $arch
@@ -35,7 +19,7 @@ CUDA_VISIBLE_DEVICES=3 python -m robustness.main \
        --epochs 105\
        --adv-train 0 \
        --adv-eval 1 \
-       --data-aug 0 \
+       --data-aug 1 \
        --tries 10 \
        --use-best 1 \
        --rot 30\
@@ -54,29 +38,7 @@ CUDA_VISIBLE_DEVICES=3 python -m robustness.main \
        --data "./path/to/cifar" \
        --batch-size 128
 done
-# python -m robustness.main \
-#        --dataset "cifar" \
-#        --epochs 110\
-#        --adv-train 1 \
-#        --adv-eval 1 \
-#        --data-aug 1 \
-#        --tries 1 \
-#        --use-best 0 \
-#        --rot 30\
-#        --trans 0.3\
-#        --scale 0.3\
-#        --hue 1.5707963268\
-#        --satu 0.3\
-#        --bright 0.3\
-#        --cont 0.3\
-#        --gau-size 11\
-#        --gau-sigma 9\
-#        --transform-type 'semantic' \
-#        --attack-type "random" \
-#        --out-dir ./training/adv/cifar/resnet18/semantic/ \
-#        --arch 'resnet18' \
-#        --data "./path/to/cifar" \
-#        --batch-size 128
+
 
 
 
